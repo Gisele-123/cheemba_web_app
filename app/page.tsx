190a7a377242453afe4e8fa7bc5700d42fdeeecb
Gisele-123 <giselemigisha53@gmail.com>
2025-01-31 09:30:28 +0200

    navigations all added successfully

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,7 @@
-import { GetServerSideProps } from 'next';
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  return {
-    redirect: {
-      destination: '/login',
-      permanent: true, // Set to true if the redirect is permanent
-    },
-  };
-};
+// app/page.tsx
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  return null; // Render nothing
+  redirect('/login');
+  return null;
 }
